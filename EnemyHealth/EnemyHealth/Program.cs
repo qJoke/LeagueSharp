a807e7563ffd15610fa2b73525832272d0d73397
DZ191 <dz191@dz191.me>
2015-09-29 17:54:50 +0200

    EnemyHealth#: Fixing Health text

--- EnemyHealth/EnemyHealth/Program.cs
+++ EnemyHealth/EnemyHealth/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using SharpDX;
@@ -33,15 +34,30 @@ namespace EnemyHealth
                 return;
             }
 
-            foreach (var enemy in HeroManager.Enemies.Where(h => h.IsVisible && !h.IsDead && h.IsHPBarRendered))
+            foreach (var enemy in HeroManager.Allies.Where(h => h.IsVisible && !h.IsDead && h.IsHPBarRendered))
             {
                 var barPosition = enemy.HPBarPosition;
                 var x = barPosition.X;
                 var y = barPosition.Y;
                 var h2 = Math.Round(enemy.Health, 0).ToString(CultureInfo.InvariantCulture);
 
-                Drawing.DrawText(x - 20, y + 5, System.Drawing.Color.Orange, h2.ToString(CultureInfo.InvariantCulture));
+                Drawing.DrawText(x - 20 - TextWidth(h2, new Font("Calibri", 4)), y + 5, System.Drawing.Color.Orange, h2.ToString(CultureInfo.InvariantCulture));
             }
+        }
+
+        public static float TextWidth(string text, Font f)
+        {
+            float textWidth;
+
+            using (var bmp = new Bitmap(1, 1))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    textWidth = g.MeasureString(text, f).Width;
+                }
+            }
+
+            return textWidth;
         }
     }
 }
