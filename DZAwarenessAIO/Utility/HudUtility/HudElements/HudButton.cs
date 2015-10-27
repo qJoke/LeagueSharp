b83ab8c61871fb8d2c72537489a9ab1a93ecbb6f
DZ191 <darkprince97@live.it>
2015-10-27 20:18:36 +0100

    DZA: More Hud work.

--- DZAwarenessAIO/Utility/HudUtility/HudElements/HudButton.cs
+++ DZAwarenessAIO/Utility/HudUtility/HudElements/HudButton.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using DZAwarenessAIO.Properties;
 using DZAwarenessAIO.Utility.Extensions;
 using LeagueSharp;
 using LeagueSharp.Common;
@@ -38,15 +40,33 @@ namespace DZAwarenessAIO.Utility.HudUtility.HudElements
         public OnButtonClickDelegate OnButtonClick { get; set; }
 
         /// <summary>
+        /// Gets or sets the button sprite.
+        /// </summary>
+        /// <value>
+        /// The button sprite.
+        /// </value>
+        public Render.Sprite ButtonSprite { get; set; }
+
+        /// <summary>
+        /// Gets or sets the button bitmap.
+        /// </summary>
+        /// <value>
+        /// The button bitmap.
+        /// </value>
+        public Bitmap ButtonBitmap { get; set; }
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="HudButton"/> class.
         /// </summary>
         /// <param name="x">The x.</param>
         /// <param name="y">The y.</param>
         /// <param name="Parent">The parent.</param>
+        /// <param name="bitmap">The Bitmap for the button</param>
         /// <param name="disanchor">if set to <c>true</c> it will use absolute positioning.</param>
-        protected HudButton(int x, int y, HudPanel Parent, bool disanchor = false)
+        protected HudButton(int x, int y, HudPanel Parent, Bitmap bitmap = null, bool disanchor = false)
         {
             this.Parent = Parent;
+            this.ButtonBitmap = bitmap;
             Position = disanchor ? 
                 new Vector2(HudVariables.CurrentPosition.X + x, HudVariables.CurrentPosition.Y + y) : 
                 new Vector2(Parent.Position.X + x, Parent.Position.Y + y);
@@ -58,10 +78,12 @@ namespace DZAwarenessAIO.Utility.HudUtility.HudElements
         /// </summary>
         /// <param name="position">The position.</param>
         /// <param name="Parent">The parent.</param>
+        /// <param name="bitmap">The button bitmap</param>
         /// <param name="disanchor"></param>
-        protected HudButton(Vector2 position, HudPanel Parent, bool disanchor = false)
+        protected HudButton(Vector2 position, HudPanel Parent, Bitmap bitmap = null, bool disanchor = false)
         {
             this.Parent = Parent;
+            this.ButtonBitmap = bitmap;
             Position = disanchor ? 
                 new Vector2(HudVariables.CurrentPosition.X + position.X, HudVariables.CurrentPosition.Y + position.Y) : 
                 new Vector2(Parent.Position.X + position.X, Parent.Position.Y + position.Y);
