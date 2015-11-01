4e1aeee4b35c5c9bc4d8f2694f4ebc66758c9149
DZ191 <darkprince97@live.it>
2015-11-01 18:44:52 +0100

    DZA: Roslyn

--- DZAwarenessAIO/Modules/Gank Alerter/GankAlerterCalculator.cs
+++ DZAwarenessAIO/Modules/Gank Alerter/GankAlerterCalculator.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Linq;
 using DZAwarenessAIO.Modules.SSTracker;
-using DZAwarenessAIO.Utility;
-using DZAwarenessAIO.Utility.HudUtility;
 using DZAwarenessAIO.Utility.Logs;
 using DZAwarenessAIO.Utility.MenuUtility;
 using LeagueSharp;
@@ -20,15 +18,22 @@ namespace DZAwarenessAIO.Modules.Gank_Alerter
         /// </summary>
         public static void OnLoad()
         {
-            GankAlerterVariables.GankAlertText.Centered = true;
-            GankAlerterVariables.GankAlertText.VisibleCondition = delegate
-            { return MenuExtensions.GetItemValue<bool>("dz191.dza.gank") && GetGankingHero() != null; };
-            GankAlerterVariables.GankAlertText.X = (int)(Drawing.Direct3DDevice.Viewport.Width / 2f);
-            GankAlerterVariables.GankAlertText.Y = (int)(9);
-            GankAlerterVariables.GankAlertText.TextUpdate = () => GetGankingHero() != null
-                ? string.Format(GetGankingHero().ChampionName + " is ganking!")
-                : string.Empty;
-            GankAlerterVariables.GankAlertText.Add(0);
+            try
+            {
+                GankAlerterVariables.GankAlertText.Centered = true;
+                GankAlerterVariables.GankAlertText.VisibleCondition = delegate
+                { return MenuExtensions.GetItemValue<bool>("dz191.dza.gank") && GetGankingHero() != null; };
+                GankAlerterVariables.GankAlertText.X = (int) (Drawing.Direct3DDevice.Viewport.Width/2f);
+                GankAlerterVariables.GankAlertText.Y = (int) (9);
+                GankAlerterVariables.GankAlertText.TextUpdate = () => GetGankingHero() != null
+                    ? string.Format(GetGankingHero().ChampionName + " is ganking!")
+                    : string.Empty;
+                GankAlerterVariables.GankAlertText.Add(0);
+            }
+            catch (Exception e)
+            {
+                LogHelper.AddToLog(new LogItem("Gank_Alerter", e, LogSeverity.Error));
+            }
         }
 
         /// <summary>
