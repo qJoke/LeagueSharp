e659d034135cbfe77acad50b5f0963544e3decef
DZ191 <darkprince97@live.it>
2015-11-23 15:20:38 +0100

    iSR: KogMaw: Base.

---
+++ iSeriesReborn/Champions/KogMaw/KogMaw.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using DZLib.Logging;
+using iSeriesReborn.Utility;
+using iSeriesReborn.Utility.MenuUtility;
+using iSeriesReborn.Utility.ModuleHelper;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace iSeriesReborn.Champions.KogMaw
+{
+    class KogMaw : ChampionBase
+    {
+        private readonly Dictionary<SpellSlot, Spell> spells = new Dictionary<SpellSlot, Spell>()
+        {
+            { SpellSlot.Q, new Spell(SpellSlot.Q) },
+            { SpellSlot.W, new Spell(SpellSlot.W) },
+            { SpellSlot.E, new Spell(SpellSlot.E) },
+            { SpellSlot.R, new Spell(SpellSlot.R) }
+        };
+
+        protected override void OnChampLoad()
+        {
+
+        }
+
+        protected override void LoadMenu()
+        {
+            var defaultMenu = Variables.Menu;
+
+            var comboMenu = defaultMenu.AddModeMenu(Orbwalking.OrbwalkingMode.Combo);
+            {
+                comboMenu.AddSkill(SpellSlot.Q, Orbwalking.OrbwalkingMode.Combo, true, 15);
+                comboMenu.AddSkill(SpellSlot.E, Orbwalking.OrbwalkingMode.Combo, true, 5);
+                comboMenu.AddSkill(SpellSlot.R, Orbwalking.OrbwalkingMode.Combo, false, 10);
+            }
+
+            var mixedMenu = defaultMenu.AddModeMenu(Orbwalking.OrbwalkingMode.Mixed);
+            {
+                mixedMenu.AddSkill(SpellSlot.Q, Orbwalking.OrbwalkingMode.Mixed, true, 15);
+                mixedMenu.AddSkill(SpellSlot.E, Orbwalking.OrbwalkingMode.Mixed, true, 5);
+            }
+
+            var laneclearMenu = defaultMenu.AddModeMenu(Orbwalking.OrbwalkingMode.LaneClear);
+            {
+                laneclearMenu.AddSkill(SpellSlot.Q, Orbwalking.OrbwalkingMode.LaneClear, true, 50);
+            }
+
+            var miscMenu = defaultMenu.AddSubMenu(new Menu("[iSR] Misc", "iseriesr.kogmaw.misc"));
+            {
+               
+            }
+        }
+
+        protected override void OnTick()
+        {
+        }
+
+        protected override void OnCombo()
+        {
+
+        }
+
+        protected override void OnMixed()
+        {
+
+        }
+
+        protected override void OnLastHit() { }
+
+        protected override void OnLaneClear()
+        {
+
+        }
+
+        protected override void OnAfterAttack(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            
+        }
+
+        public override Dictionary<SpellSlot, Spell> GetSpells()
+        {
+            return spells;
+        }
+
+        public override List<IModule> GetModules()
+        {
+            return new List<IModule>()
+            {
+                
+            };
+        }
+    }
+}
