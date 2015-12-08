fce61843a0b019ea17956180162b935e89219327
DZ191 <darkprince97@live.it>
2015-12-08 18:37:49 +0100

    SOLO Vayne: Major improvements to Q logic for all cases situation.

--- SoloVayne/SoloVayne/Skills/Tumble/TumbleLogicProvider.cs
+++ SoloVayne/SoloVayne/Skills/Tumble/TumbleLogicProvider.cs
@@ -3,7 +3,6 @@ using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
 using SoloVayne.Utility;
-using SoloVayne.Utility.Entities;
 
 namespace SoloVayne.Skills.Tumble
 {
@@ -11,6 +10,7 @@ namespace SoloVayne.Skills.Tumble
     {
         public Vector3 GetSOLOVayneQPosition()
         {
+            #region The Required Variables
             var positions = TumbleHelper.GetRotatedQPositions();
             var enemyPositions = TumbleHelper.GetEnemyPoints();
             var safePositions = positions.Where(pos => !enemyPositions.Contains(pos.To2D())).ToList();
@@ -20,37 +20,52 @@ namespace SoloVayne.Skills.Tumble
 
             var bestWeightedAvg = 0f;
 
+            var highHealthEnemiesNear =
+                    HeroManager.Enemies.Where(m => !m.IsMelee && m.IsValidTarget(1300f) && m.HealthPercent > 7).ToList();
+
+            var alliesNear = HeroManager.Allies.Count(ally => !ally.IsMe && ally.IsValidTarget(1500f, false));
+
+            var enemiesNear =
+                HeroManager.Enemies.Where(m => m.IsValidTarget(Orbwalking.GetRealAutoAttackRange(m) + 300f + 65f)).ToList();
+            #endregion
+
+            #region 1 Enemy around only
             if (ObjectManager.Player.CountEnemiesInRange(1500f) <= 1)
             {
                 //Logic for 1 enemy near
                 var position = ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 300f);
                 return position.IsSafeEx() ? position : Vector3.Zero;
             }
+            #endregion
 
-            var targettingEnemies =
-                    HeroManager.Enemies.Where(m => !m.IsMelee && m.IsValidTarget(1300f) && m.HealthPercent > 7).ToList();
-            
-            if (HeroManager.Allies.Count(ally => !ally.IsMe && ally.IsValidTarget(1500f, false)) < 1 &&
-                targettingEnemies.Count() <= 2)
+            #region Alone, 2 Enemies, 1 Killable
+            if (alliesNear == 0 && enemiesNear.Count() <= 2)
             {
-                //Logic for 2 enemies Near and alone
                 if (
-                    targettingEnemies.Any(
+                    enemiesNear.Any(
                         t =>
                             t.Health + 15 <
-                            ObjectManager.Player.GetAutoAttackDamage(t) + Variables.spells[SpellSlot.Q].GetDamage(t) 
+                            ObjectManager.Player.GetAutoAttackDamage(t) + Variables.spells[SpellSlot.Q].GetDamage(t)
                             && t.Distance(ObjectManager.Player) < Orbwalking.GetRealAutoAttackRange(t) + 80f))
                 {
                     var QPosition =
                         ObjectManager.Player.ServerPosition.Extend(
-                            targettingEnemies.OrderBy(t => t.Health).First().ServerPosition, 300f);
+                            highHealthEnemiesNear.OrderBy(t => t.Health).First().ServerPosition, 300f);
 
                     if (!QPosition.UnderTurret(true))
                     {
                         return QPosition;
                     }
                 }
+            }
+            #endregion
 
+            #region Alone, 2 Enemies, None Killable
+            if (alliesNear == 0 && highHealthEnemiesNear.Count() <= 2)
+            {
+                //Logic for 2 enemies Near and alone
+
+                //If there is a killable enemy among those. 
                 var backwardsPosition = (ObjectManager.Player.ServerPosition.To2D() + 300f * ObjectManager.Player.Direction.To2D()).To3D();
 
                 if (!backwardsPosition.UnderTurret(true))
@@ -58,16 +73,22 @@ namespace SoloVayne.Skills.Tumble
                     return backwardsPosition;
                 }
             }
+            #endregion
 
-            var closeEnemy = TumbleHelper.GetClosestEnemy(ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 300f));
+            #region Already in an enemy's attack range. 
+            var closeNonMeleeEnemy = TumbleHelper.GetClosestEnemy(ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 300f));
 
-            if (ObjectManager.Player.Distance(closeEnemy) <= closeEnemy.AttackRange - 85 && !closeEnemy.IsMelee)
+            if (closeNonMeleeEnemy != null 
+                && ObjectManager.Player.Distance(closeNonMeleeEnemy) <= closeNonMeleeEnemy.AttackRange - 85 
+                && !closeNonMeleeEnemy.IsMelee)
             {
                 return ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 300f).IsSafeEx()
                     ? ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 300f)
                     : Vector3.Zero;
             }
+            #endregion
 
+            #region Logic for multiple enemies / allies around.
             foreach (var position in safePositions)
             {
                 var enemy = TumbleHelper.GetClosestEnemy(position);
@@ -75,14 +96,6 @@ namespace SoloVayne.Skills.Tumble
                 {
                     continue;
                 }
-
-                if (ObjectManager.Player.Distance(enemy) <= enemy.AttackRange - 85 && !enemy.IsMelee)
-                {
-                    return ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 300f).IsSafeEx()
-                        ? ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 300f)
-                        : Vector3.Zero;
-                }
-
 
                 var avgDist = TumbleHelper.GetAvgDistance(position);
 
@@ -97,9 +110,50 @@ namespace SoloVayne.Skills.Tumble
                     }
                 }
             }
-            
-            
-            return (BestPosition.IsSafe()) ? BestPosition : Vector3.Zero;
+            #endregion
+
+            var endPosition = (BestPosition.IsSafe()) ? BestPosition : Vector3.Zero;
+
+            #region Couldn't find a suitable position, tumble to nearest ally logic
+            if (endPosition == Vector3.Zero)
+            {
+                //Try to find another suitable position. This usually means we are already near too much enemies turrets so just gtfo and tumble
+                //to the closest ally ordered by most health.
+                var alliesClose = HeroManager.Allies.Where(ally => !ally.IsMe && ally.IsValidTarget(1500f, false)).ToList();
+                if (alliesClose.Any() && enemiesNear.Any())
+                {
+                    var closestMostHealth =
+                    alliesClose.OrderBy(m => m.Distance(ObjectManager.Player)).ThenByDescending(m => m.Health).FirstOrDefault();
+
+                    if (closestMostHealth != null 
+                        && closestMostHealth.Distance(enemiesNear.OrderBy(m => m.Distance(ObjectManager.Player)).FirstOrDefault()) 
+                        > ObjectManager.Player.Distance(enemiesNear.OrderBy(m => m.Distance(ObjectManager.Player)).FirstOrDefault()))
+                    {
+                        var tempPosition = ObjectManager.Player.ServerPosition.Extend(closestMostHealth.ServerPosition,
+                            300f);
+                        if (tempPosition.IsSafeEx())
+                        {
+                            endPosition = tempPosition;
+                        }
+                    }
+                    
+                }
+
+            }
+            #endregion
+
+            #region Couldn't even tumble to ally, just go to mouse
+            if (endPosition == Vector3.Zero)
+            {
+                var mousePosition = ObjectManager.Player.ServerPosition.Extend(Game.CursorPos, 300f);
+                if (mousePosition.IsSafe())
+                {
+                    endPosition = mousePosition;
+                }
+            }
+            #endregion
+
+            return endPosition;
         }
     }
 }
