cd1fb9fda97ade091d7b5c921b921a8d845660ea
DZ191 <darkprince97@live.it>
2016-04-02 18:22:52 +0200

    DZLib: More SafePositioning Work.

--- DZLib/Positioning/PositioningHelper.cs
+++ DZLib/Positioning/PositioningHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DZLib.Geometry;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
@@ -7,6 +10,18 @@ namespace DZLib.Positioning
 {
     class PositioningHelper
     {
+        public static IEnumerable<Obj_AI_Hero> EnemiesClose
+        {
+            get
+            {
+                return
+                    HeroManager.Enemies.Where(
+                        m =>
+                            m.Distance(ObjectManager.Player, true) <= Math.Pow(1000, 2) && m.IsValidTarget(1500, false) &&
+                            m.CountEnemiesInRange(m.IsMelee() ? m.AttackRange * 1.5f : m.AttackRange + 20 * 1.5f) > 0);
+            }
+        }
+
         public static float GetAvgDistanceFromEnemyTeam(Vector3 from, float MaxRange = 1200f)
         {
             var numberOfEnemies = from.CountEnemiesInRange(MaxRange);
@@ -48,5 +63,27 @@ namespace DZLib.Positioning
                .FirstOrDefault(ally => !ally.IsMe && ally.IsValidTarget(MaxRange, false, from == default(Vector3) ? ObjectManager.Player.ServerPosition : from));
         }
 
+        public static List<Vector2> GetRotatedPositions(Vector3 from, float range, float angle, int step)
+        {
+            int currentStep = step;
+            var direction = ObjectManager.Player.Direction.To2D().Perpendicular();
+            var currentList = new List<Vector2>();
+            for (var i = 0f; i < angle; i += currentStep)
+            {
+                var angleRad = LeagueSharp.Common.Geometry.DegreeToRadian(i);
+                var rotatedPosition = ObjectManager.Player.Position.To2D() + (range * direction.Rotated(angleRad));
+                currentList.Add(rotatedPosition);
+            }
+            return currentList;
+        }
+
+        public static List<Vector2> GetEnemyZoneList(bool dynamic = true)
+        {
+            var staticRange = 360f;
+            var polygonsList = EnemiesClose.ToList().Select(enemy => new DZGeometry.Circle(enemy.ServerPosition.To2D(), (dynamic ? (enemy.IsMelee ? enemy.AttackRange * 1.5f : enemy.AttackRange) : staticRange) + enemy.BoundingRadius + 20).ToPolygon()).ToList();
+            var pathList = DZGeometry.ClipPolygons(polygonsList);
+            var pointList = pathList.SelectMany(path => path, (path, point) => new Vector2(point.X, point.Y)).Where(currentPoint => !currentPoint.IsWall()).ToList();
+            return pointList;
+        }
     }
 }
--- DZLib/Positioning/SafePosition.cs
+++ DZLib/Positioning/SafePosition.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DZLib.Hero;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
 
 namespace DZLib.Positioning
 {
-    class SafePosition
+    static class SafePosition
     {
+        public static bool IsSafe(this Vector3 position, float range)
+        {
+            if (position.UnderTurret(true) && !ObjectManager.Player.UnderTurret(true))
+            {
+                return false;
+            }
+
+            var allies = position.CountAlliesInRange(ObjectManager.Player.AttackRange);
+            var enemies = position.CountEnemiesInRange(ObjectManager.Player.AttackRange);
+            var lhEnemies = position.GetLhEnemiesNear(ObjectManager.Player.AttackRange, 15).Count();
+
+            if (enemies <= 1) ////It's a 1v1, safe to assume I can Q
+            {
+                return true;
+            }
+
+            if (position.UnderAllyTurret())
+            {
+                var nearestAllyTurret = ObjectManager.Get<Obj_AI_Turret>().Where(a => a.IsAlly).OrderBy(d => d.Distance(position, true)).FirstOrDefault();
+
+                if (nearestAllyTurret != null)
+                {
+                    ////We're adding more allies, since the turret adds to the firepower of the team.
+                    allies += 2;
+                }
+            }
+
+            var normalCheck = (allies + 1 > enemies - lhEnemies);
+
+            return normalCheck;
+        }
     }
 }
