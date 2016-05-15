f74286022236d302dcd90cdc272c7f70204b2d93
DZ191 <darkprince97@live.it>
2016-05-15 13:03:34 +0200

    DZAIO: Derping VS.

---
+++ DZAIO_Reborn/DZAIO_Reborn/DZAIO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DZAIO_Reborn.Core;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace DZAIO_Reborn
+{
+    class DZAIO
+    {
+        public static void Init()
+        {
+            Game.OnUpdate += OnTick;
+        }
+
+        private static void OnTick(EventArgs args)
+        {
+            if (Variables.CurrentChampion != null && Variables.Orbwalker != null)
+            {
+                Variables.CurrentChampion.OnTick();
+
+                switch (Variables.Orbwalker.ActiveMode)
+                {
+                    case Orbwalking.OrbwalkingMode.Combo:
+                        Variables.CurrentChampion.OnCombo();
+                        break;
+                    case Orbwalking.OrbwalkingMode.Mixed:
+                        Variables.CurrentChampion.OnMixed();
+                        break;
+                    case Orbwalking.OrbwalkingMode.LastHit:
+                        Variables.CurrentChampion.OnLastHit();
+                        break;
+                    case Orbwalking.OrbwalkingMode.LaneClear:
+                        Variables.CurrentChampion.OnLaneclear();
+                        break;
+                }
+            }
+            
+        }
+    }
+}
