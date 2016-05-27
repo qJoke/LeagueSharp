1075c6fef7ca976e464f9e3863b74c4acea0e05b
DZ191 <darkprince97@live.it>
2016-05-27 20:16:25 +0200

    DZAIO: Ahri Don't wait for Charm Combo.

--- DZAIO_Reborn/DZAIO_Reborn/Plugins/Champions/Ahri/Ahri.cs
+++ DZAIO_Reborn/DZAIO_Reborn/Plugins/Champions/Ahri/Ahri.cs
@@ -149,7 +149,25 @@ namespace DZAIO_Reborn.Plugins.Champions.Ahri
                 }
                 else
                 {
-                    
+                    if (Variables.Spells[SpellSlot.E].IsEnabledAndReady(ModesMenuExtensions.Mode.Combo) &&
+                        Variables.Spells[SpellSlot.Q].IsEnabledAndReady(ModesMenuExtensions.Mode.Combo) &&
+                        !finalTarget.IsCharmed)
+                    {
+                        Variables.Spells[SpellSlot.E].CastIfHitchanceEquals(finalTarget, HitChance.High);
+                    }
+
+                    if (Variables.Spells[SpellSlot.Q].IsEnabledAndReady(ModesMenuExtensions.Mode.Combo))
+                    {
+                        Variables.Spells[SpellSlot.Q].CastIfHitchanceEquals(finalTarget, HitChance.High);
+                    }
+
+                    if (Variables.Spells[SpellSlot.W].IsEnabledAndReady(ModesMenuExtensions.Mode.Combo) &&
+                        finalTarget.IsValidTarget(Variables.Spells[SpellSlot.W].Range) &&
+                        ((Variables.Spells[SpellSlot.W].GetDamage(finalTarget) +
+                          Variables.Spells[SpellSlot.Q].GetDamage(finalTarget) > finalTarget.Health + 25)))
+                    {
+                        Variables.Spells[SpellSlot.W].Cast();
+                    }
                 }
 
                 
