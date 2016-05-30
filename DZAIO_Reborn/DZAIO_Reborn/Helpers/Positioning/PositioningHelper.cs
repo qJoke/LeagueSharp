637df50945d8d830f2884896b1e2e2f7eeb35236
DZ191 <darkprince97@live.it>
2016-05-30 19:11:33 +0200

    DZAIO: Geometry class and IsSafe Method.

---
+++ DZAIO_Reborn/DZAIO_Reborn/Helpers/Positioning/PositioningHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace DZAIO_Reborn.Helpers.Positioning
+{
+    static class PositioningHelper
+    {
+        internal static bool IsSafe(this Vector3 Position)
+        {
+            if ((Position.UnderTurret(true) 
+                && !ObjectManager.Player.UnderTurret(true)) 
+                || (PositioningVariables.EnemiesClose.Count() > 1 && DZAIOGeometry.GetEnemyPoints().Contains(Position.To2D())))
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+    }
+}
