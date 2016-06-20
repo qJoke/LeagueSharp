20c3dfc0b3df2199088d27920dbbc8778053b0d3
DZ191 <darkprince97@live.it>
2016-06-20 18:32:40 +0200

    DZAIO: Orianna Base Class and CommandQueue / Strings

---
+++ DZAIO_Reborn/DZAIO_Reborn/Plugins/Champions/Orianna/Orianna.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DZAIO_Reborn.Core;
+using DZAIO_Reborn.Helpers;
+using DZAIO_Reborn.Helpers.Entity;
+using DZAIO_Reborn.Helpers.Modules;
+using DZAIO_Reborn.Plugins.Champions.Orianna.BallManager;
+using DZAIO_Reborn.Plugins.Champions.Veigar.Modules;
+using DZAIO_Reborn.Plugins.Interface;
+using DZLib.Core;
+using DZLib.Menu;
+using DZLib.MenuExtensions;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SPrediction;
+
+namespace DZAIO_Reborn.Plugins.Champions.Orianna
+{
+    class Orianna : IChampion
+    {
+        public PetHandler 
+        public void OnLoad(Menu menu)
+        {
+            var comboMenu = new Menu(ObjectManager.Player.ChampionName + ": Combo", "dzaio.champion.orianna.combo");
+            {
+                comboMenu.AddModeMenu(ModesMenuExtensions.Mode.Combo, new[] { SpellSlot.Q, SpellSlot.W, SpellSlot.E, SpellSlot.R }, new[] { true, true, true, true });
+                //comboMenu.AddNoUltiMenu(false);
+                menu.AddSubMenu(comboMenu);
+            }
+
+            var mixedMenu = new Menu(ObjectManager.Player.ChampionName + ": Mixed", "dzaio.champion.orianna.harrass");
+            {
+                mixedMenu.AddModeMenu(ModesMenuExtensions.Mode.Harrass, new[] { SpellSlot.Q, SpellSlot.W, SpellSlot.E }, new[] { true, true, true });
+                mixedMenu.AddSlider("dzaio.champion.veigar.mixed.mana", "Min Mana % for Harass", 30, 0, 100);
+                menu.AddSubMenu(mixedMenu);
+            }
+
+            var farmMenu = new Menu(ObjectManager.Player.ChampionName + ": Farm", "dzaio.champion.orianna.farm");
+            {
+                farmMenu.AddModeMenu(ModesMenuExtensions.Mode.Laneclear, new[] { SpellSlot.Q, SpellSlot.W }, new[] { true, true });
+
+                farmMenu.AddBool("dzaio.champion.veigar.farm.w.kill", "Only Use W to kill Minions", false);
+                farmMenu.AddSlider("dzaio.champion.veigar.farm.w.min", "Min Minions for W", 2, 1, 6);
+                farmMenu.AddSlider("dzaio.champion.veigar.farm.mana", "Min Mana % for Farm", 30, 0, 100);
+                menu.AddSubMenu(farmMenu);
+            }
+
+            var extraMenu = new Menu(ObjectManager.Player.ChampionName + ": Extra", "dzaio.champion.orianna.extra");
+            {
+                extraMenu.AddBool("dzaio.champion.veigar.extra.interrupter", "Interrupter (R)", true);
+            }
+
+            Variables.Spells[SpellSlot.Q].SetSkillshot(0f, 110f, 1425f, false, SkillshotType.SkillshotLine);
+            Variables.Spells[SpellSlot.E].SetSkillshot(0.25f, 80f, 1700f, true, SkillshotType.SkillshotLine);
+            CommandQueue.InitEvents();
+
+        }
+
+        public void RegisterEvents()
+        {
+            DZInterrupter.OnInterruptableTarget += OnInterrupter;
+            DZAntigapcloser.OnEnemyGapcloser += OnGapcloser;
+        }
+
+        private void OnGapcloser(DZLib.Core.ActiveGapcloser gapcloser)
+        {
+            
+        }
+
+        private void OnInterrupter(Obj_AI_Hero sender, DZInterrupter.InterruptableTargetEventArgs args)
+        {
+            
+        }
+        public Dictionary<SpellSlot, Spell> GetSpells()
+        {
+            return new Dictionary<SpellSlot, Spell>
+                      {
+                                    { SpellSlot.Q, new Spell(SpellSlot.Q, 800f) },
+                                    { SpellSlot.W, new Spell(SpellSlot.W, 1000f) },
+                                    { SpellSlot.E, new Spell(SpellSlot.E, 700f) },
+                                    { SpellSlot.R, new Spell(SpellSlot.R, 640f) }
+                      };
+        }
+
+        public List<IModule> GetModules()
+        {
+            return new List<IModule>()
+            {
+                
+            };
+        }
+
+        public void OnTick()
+        {
+
+        }
+
+        public void OnCombo()
+        {
+            
+        }
+
+        public void OnMixed()
+        {
+            
+        }
+
+        public void OnLastHit()
+        { }
+
+        public void OnLaneclear()
+        {
+            
+        }
+    }
+}
