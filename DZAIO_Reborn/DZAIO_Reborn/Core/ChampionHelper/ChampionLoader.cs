de24962e5858be6acd4856e68b0ea837ad6f414c
DZ191 <darkprince97@live.it>
2016-06-28 19:20:10 +0200

    DZAIO: "Also Try It With" feature

--- DZAIO_Reborn/DZAIO_Reborn/Core/ChampionHelper/ChampionLoader.cs
+++ DZAIO_Reborn/DZAIO_Reborn/Core/ChampionHelper/ChampionLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace DZAIO_Reborn.Core.ChampionHelper
 {
     class ChampionLoader
     {
+         private static Random random = new Random();
+
         public static void LoadChampion()
         {
             if (Variables.ChampList.ContainsKey(ObjectManager.Player.ChampionName))
@@ -16,8 +19,24 @@ namespace DZAIO_Reborn.Core.ChampionHelper
                 Variables.CurrentChampion = Variables.ChampList[ObjectManager.Player.ChampionName].Invoke();
                 Variables.CurrentChampion.OnLoad(Variables.AssemblyMenu);
                 Variables.CurrentChampion.RegisterEvents();
-                Game.PrintChat("<b><font color='#FF0000'>[DZAIO: Reborn] </font></b><font color='#FFFFFF'>Loaded</font> <b><font color='#FF0000'>{0}</font></b> plugin! <font color='#FFFFFF'> Have fun! </font>", ObjectManager.Player.ChampionName);
+                Game.PrintChat("<b><font color='#FF0000'>[DZAIO: Reborn] </font></b><font color='#FFFFFF'>Loaded</font> <b><font color='#FF0000'>{0}</font></b> plugin!", ObjectManager.Player.ChampionName);
+
+                var champString = "<b>Also try it with: </b>";
+
+                foreach (var champKvp in Variables.ChampList.Where(n => n.Key != ObjectManager.Player.ChampionName))
+                {
+                    champString += "<b><font color='" + GetRandomColor() +"'>" + champKvp.Key + "</font></b><font color='#FFFFFF'>, </font>";
+                }
+                    
+                Game.PrintChat(champString);
             }
+        }
+
+        public static string GetRandomColor()
+        {
+            var color = $"#{random.Next(0x1000000):X6}";
+
+            return color;
         }
     }
 }
